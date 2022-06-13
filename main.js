a09d71e57deaaf07a0b973ad58178f180574be37
Vlad Starzhitskiy <study.html.css.java@gmail.com>
2022-06-13 11:15:41 +0400

    done

---
+++ main.js
@@ -0,0 +1,35 @@
+const requestUrl = 'http://api.tvmaze.com/search/shows?q=cars';
+const dataWrapper = document.getElementById('data-wrapper');
+
+const createTemplate = data => {
+  let genres = [];
+  if(data.show.genres.length) {
+    genres = data.show.genres.reduce((acc,item) => {
+      return acc + ", " + item;
+    })
+  } else {
+    genres = 'unknown'
+  }
+  return `
+    <div class="data-item">
+      <div class="image">
+        <img src="${data.show.image ? data.show.image.medium : ''}" alt="image">
+      </div>
+      <div><span>Name:</span>${data.show.name}</div>
+      <div><span>Score:</span>${data.score}</div>
+      <div><span>Genres:</span>${genres}</div>
+      <div><span>Language:</span>${data.show.language}</div>
+      <div><span>Description:</span>${data.show.sumary}</div>
+    </div>
+  `
+}
+
+fetch(requestUrl)
+  .then(response => response.json())
+  .then(data => {
+    if(data) {
+      data.forEach(item => {
+        dataWrapper.innerHTML += createTemplate(item)
+      })
+    }
+  })
